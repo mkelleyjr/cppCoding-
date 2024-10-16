67c4b2f8b026df43cafd21e57b0694e12749254a
mkelleyjr <michael.kelley.jr@gmail.com>
2024-10-16 12:50:15 -0400

    return 0

---
+++ cppCoding/cppCoding/helloWorld.cpp
@@ -0,0 +1,16 @@
+//
+//  helloWorld.cpp
+//  cppCoding
+//
+//  Created by Michael Kelley Jr. on 10/16/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World from Xcode!" << endl;
+    
+    return 0; 
+}
