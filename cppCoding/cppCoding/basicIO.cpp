c5d8ea47bac2e299417f7d7934385210f86049e3
mkelleyjr <michael.kelley.jr@gmail.com>
2024-10-17 21:43:14 -0400

    new file for IO

---
+++ cppCoding/cppCoding/basicIO.cpp
@@ -0,0 +1,22 @@
+//
+//  basicIO.cpp
+//  cppCoding
+//
+//  Created by Michael Kelley Jr. on 10/17/24.
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter 2 numbers:" << endl;
+    
+    // Get 2 integers from input 
+    int v1 = 0, v2 = 0;
+    cin >> v1 >> v2;
+    // Output the sum 
+    cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << endl;
+
+    return 0; 
+}
