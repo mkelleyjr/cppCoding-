e098a84b12fb3470d15fa00fec215c7e78228c71
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2024-10-28 17:55:59 -0400

    started math program.

---
+++ cppCoding/cppCoding/math.cpp
@@ -0,0 +1,22 @@
+//
+//  math.cpp
+//  cppCoding
+//
+//  Created by Michael Kelley Jr. on 10/28/24.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Below are some math operations in C++" << endl;
+
+    int sum = 0;
+    int a = 1; 
+    int b = 2;
+    int c = 3;
+
+    sum = a + b + c;
+
+    cout << "The sum of a, b, and c is " << sum << endl;
+}
