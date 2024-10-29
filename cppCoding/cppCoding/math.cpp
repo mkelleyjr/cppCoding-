f24ec10cb9cff2e314f34c198d42fa8ea83cb7e3
Michael L. Kelley Jr. <michael.kelley.jr@gmail.com>
2024-10-29 01:52:54 -0400

    updated file.

--- cppCoding/cppCoding/math.cpp
+++ cppCoding/cppCoding/math.cpp
@@ -12,11 +12,14 @@ int main()
     cout << "Below are some math operations in C++" << endl;
 
     int sum = 0;
+    int sumTwo; 
     int a = 1; 
     int b = 2;
     int c = 3;
 
     sum = a + b + c;
+    sumTwo = c - b - a; 
 
     cout << "The sum of a, b, and c is " << sum << endl;
+    cout << "C - B - A is " << sumTwo << endl; 
 }
